91c3d200139767cfa2f9ecb63c398636d8894899
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-01-01 23:39:17 +0300

    iterative solution for rev. link.list.

--- 06_linked_list.py
+++ 06_linked_list.py
@@ -7,26 +7,46 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __repr__(self) -> str:
+        return f"{self.val}"
+
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
         206. Reverse Linked List
         """
+
         # iterative solution
+        """
+                1 -> 2 -> 3 -> 4 -> 5 -> None
+        None <- 1    2 -> 3 -> 4 -> 5 -> None  
+        prev   curr
+        ...
+        None <- 1 <- 2 <- 3 <- 4 <- 5 <- None
+                                   prev  curr
+        """
         prev, curr = None, head
         while curr:
-            next = curr.next
+            temp = curr.next  # save erased arrow
             curr.next = prev
             prev = curr
-            curr = next
+            curr = temp
+
         return prev
 
 
 if __name__ == "__main__":
     s = Solution()
 
-    s.reverseList([1, 2, 3, 4, 5])
-    # print(s.reverseList(ListNode[1, 2, 3, 4, 5]))
-    # print(s.reverseList(ListNode[1, 2]))
-    # print(s.reverseList(ListNode[]))
+    head = [1, 2, 3, 4, 5]
+
+    nodes = []
+    nodes.append(ListNode(head[0]))
+    for i in range(1, len(head)):
+        nodes.append(ListNode(head[i]))
+        nodes[i - 1].next = nodes[i]
+
+    # print(nodes[0], nodes[0].next)
+    rev = s.reverseList(nodes[0])
+    print(rev, rev.next)
